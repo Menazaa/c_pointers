1945717f23d085b8902fec6820acb45de00d3b42
mohamed <myyli1999@gmail.com>
2021-09-03 07:38:08 +0200

    add pointer basics file and exerise files

--- pointer basics/pointer_basics.c
+++ pointer basics/pointer_basics.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 
-int main(int argc, char const *argv[])
+int main()
 {
-    printf("this first commit");
-    return 0;
+	char a;
+	char *ptr; /* pointer declaration. NOTE : pointers con't be used without initialization as it could
+                  crash the program and the entire system  */
+    char **ptrptr;  // pointer to a pointer variable
+
+
+	/* initialize variables! */
+	a = 'A';
+
+	printf("Value of variable 'a': %c\n",a);
+	/* assign ptr to a's address */
+	ptr = &a;   
+    // assign ptrptr to ptr's address
+    ptrptr = &ptr;
+	printf("Address of variable 'a': %p\n",ptr);
+    printf("Address of the pointer \"ptr\": %p\n",ptrptr);
+	printf("Value of pointer 'ptr': %c\n",*ptr);
+    printf("Value of variable ptrptr: %p\n",*ptrptr);
+
+	return(0);
 }
