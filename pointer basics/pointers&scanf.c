9a64925df8308f13744caf353822454ccb0fc768
mohamed <myyli1999@gmail.com>
2021-09-03 08:23:10 +0200

    create pointer and scanf example

---
+++ pointer basics/pointers&scanf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+
+int main(int argc, char const *argv[])
+{
+    int num;
+    scanf("%d",&num); // the second parameter of scanf is a pointer where the value will be stored
+    printf("the value of num: %d\n",num);
+    return 0;
+}
