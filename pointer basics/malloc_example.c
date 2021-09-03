01cbd21a6147ec759fb7b929ad184b8c49c8cb1c
mohamed <myyli1999@gmail.com>
2021-09-03 09:26:43 +0200

    malloc funtion example

---
+++ pointer basics/malloc_example.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	void *chunk;
+
+	chunk = malloc(1000000000);
+	/* test pointer allocation */
+	if( chunk==NULL )
+	{
+		puts("Memory allocation failed!");
+	}
+	else
+	{
+		printf("1000000000 bytes allocated at %p\n",chunk);
+		free(chunk);
+	}
+
+	return(0);
+}
